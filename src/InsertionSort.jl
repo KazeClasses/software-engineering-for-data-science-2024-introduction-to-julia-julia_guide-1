cff0b373f4dd3c7882735f0f3e71e909bccd582b
caiodeberaldini <caio-soad@hotmail.com>
2024-09-19 19:47:32 -0400

    Building insertion sort package

--- src/InsertionSort.jl
+++ src/InsertionSort.jl
@@ -1,36 +1,3 @@
 module InsertionSort
-
-using Test
-using Random
-
-function insertion_sort!(array::Array{Int, 1})
-    array_size = size(array)[1]
-    unsorted = true
-
-    while unsorted
-        swapped = false
-
-        for i in 1:array_size - 1
-            if array[i+1] < array[i]
-                array[i], array[i+1] = array[i+1], array[i]
-                swapped = true
-            end
-        end
-
-        if !swapped
-            unsorted = false
-        end
-    end
-
-    return array
-end
-
-function check_sort(N::Int = 100)
-    array = collect(1:N)
-    shuffle!(array)
-    insertion_sort!(array)
-    return array == collect(1:N)
-end
-
-export insertion_sort!, check_sort
+include("utils.jl")
 end
---
+++ src/utils.jl
@@ -0,0 +1,41 @@
+"""
+Insertion sort algorithm.
+This function sorts an array using the insertion sort algorithm.
+# Arguments
+- `arr::Vector{Int}`: The array to be sorted.
+# Examples
+julia
+arr = [3, 2, 1]
+insertion_sort!(arr)
+println(arr) \# [1, 2, 3]
+"""
+function insertion_sort!(array::Array{Int, 1})
+    array_size = size(array)[1]
+    unsorted = true
+
+    while unsorted
+        swapped = false
+
+        for i in 1:array_size - 1
+            if array[i+1] < array[i]
+                array[i], array[i+1] = array[i+1], array[i]
+                swapped = true
+            end
+        end
+
+        if !swapped
+            unsorted = false
+        end
+    end
+
+    return array
+end
+
+function check_sort(N::Int = 100)
+    array = collect(1:N)
+    shuffle!(array)
+    insertion_sort!(array)
+    return array == collect(1:N)
+end
+
+export insertion_sort!
