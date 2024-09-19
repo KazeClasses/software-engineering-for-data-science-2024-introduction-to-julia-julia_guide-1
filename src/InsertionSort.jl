deaef2cc2df6e611dceed0f8137f1e3e2e983583
caiodeberaldini <caio-soad@hotmail.com>
2024-09-19 19:03:12 -0400

    Implementation of insertion sort

--- src/InsertionSort.jl
+++ src/InsertionSort.jl
@@ -4,7 +4,25 @@ using Test
 using Random
 
 function insertion_sort!(array::Array{Int, 1})
-    # Insert your code here
+    array_size = size(array)[1]
+    unsorted = true
+
+    while unsorted
+        swapped = false
+
+        for i in 1:array_size - 1
+            if array[i+1] < array[i]
+                array[i], array[i+1] = array[i+1], array[i]
+                swapped = true
+            end
+        end
+
+        if !swapped
+            unsorted = false
+        end
+    end
+
+    return array
 end
 
 function check_sort(N::Int = 100)
