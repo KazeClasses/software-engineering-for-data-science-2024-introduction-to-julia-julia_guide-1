a1f3c2047dd791574e1dc805b5f0aee39fb1002e
EricAlanSimon <esimon10@jhu.edu>
2024-09-19 19:39:31 -0400

    package work julia

--- src/InsertionSort.jl
+++ src/InsertionSort.jl
@@ -1,28 +1,4 @@
 module InsertionSort
-
-using Test
-using Random
-
-function insertion_sort!(array::Array{Int, 1})
-    for i in 2:length(array)
-        key = array[i]
-        j = i - 1
-        
-        while j >= 1 && array[j] > key
-            array[j + 1] = array[j]
-            j -= 1
-        end
-        
-        array[j + 1] = key
-    end
+include("util.jl")
 end
 
-function check_sort(N::Int = 100)
-    array = collect(1:N)
-    shuffle!(array)
-    insertion_sort!(array)
-    return array == collect(1:N)
-end
-
-export insertion_sort!, check_sort
-end
